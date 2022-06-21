c7ea86553580b87165f2b007f55f4656fc869a87
David <kingdice@Atomic>
2022-06-21 15:36:29 +0300

    we then create a new activity for our launch or splash screen activity.

---
+++ app/src/main/java/com/example/dexterphones/ui/Launch.java
@@ -0,0 +1,19 @@
+package com.example.dexterphones.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.dexterphones.R;
+
+public class Launch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        setContentView(R.layout.activity_launch);
+    }
+}
