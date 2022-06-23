12aa7b56569c64e1756e23085c0171addce48129
Kemunto Gift <giftoyugi@gmail.com>
2022-06-23 10:19:54 +0300

    empty main activity and layout

--- app/src/main/java/com/example/dexterphones/MainActivity.java
+++ app/src/main/java/com/example/dexterphones/MainActivity.java
@@ -2,41 +2,18 @@ package com.example.dexterphones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    @BindView(R.id.findPhonesButton)
-    Button mFindPhonesButton;
-    @BindView(R.id.phoneEditText)
-    EditText mPhoneEditText;
-    @BindView(R.id.appNameTextView)
-    TextView mAppNameTextView;
+public class MainActivity extends AppCompatActivity{
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        mFindPhonesButton.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v){
-        if (v == mFindPhonesButton) {
-            String query = mPhoneEditText.getText().toString();
-            Intent intent = new Intent(MainActivity.this, PhoneListActivity.class);
-            intent.putExtra("query", query);
-            startActivity(intent);
-        }
-    }
 }
